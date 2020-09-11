db43cb6adeb688b84f922eb1b52fa42293723c31
Kadwe Pallavi <KadwePallavi@JohnDeere.com>
2020-09-11 14:45:49 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,29 @@
 console.log("Up and running!");
 
 
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+rank:"queen",
+suite:"hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{
+rank:"queen",
+suite:"diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+{
+rank:"king",
+suite:"hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{
+rank:"king",
+suite:"diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = [];
 
 function checkForMatch(){
@@ -14,9 +36,11 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suite);
+	console.log("User flipped " + cards[cardId].cardImage);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 }
