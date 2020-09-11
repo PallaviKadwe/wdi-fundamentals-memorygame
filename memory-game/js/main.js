b0345906285b836cba1f6bb7252a24d83f9b99f9
Kadwe Pallavi <KadwePallavi@JohnDeere.com>
2020-09-10 21:44:25 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,25 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User flipped " + cardOne);
+
+let cards = ["queen", "king", "king", "king"];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
+
+if(cardsInPlay.length===2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.");
+	}
+}
+
+
